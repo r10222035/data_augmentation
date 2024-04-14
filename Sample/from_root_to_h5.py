293fee2ae4d412d9e0131e81cfd38a14f954e6de
Feng-Yang Hsieh <f10222035@ntu.edu.tw>
2024-04-14 12:54:20 +0800

    Code: Implement multi-core processing for from_root_to_h5.py

--- Sample/from_root_to_h5.py
+++ Sample/from_root_to_h5.py
@@ -6,9 +6,12 @@
 import sys
 import ROOT
 import h5py
+
 import numpy as np
+import multiprocessing as mp
 
 from tqdm import tqdm
+from itertools import repeat
 
 ROOT.gROOT.ProcessLine('.include /usr/local/Delphes-3.4.2/')
 ROOT.gROOT.ProcessLine('.include /usr/local/Delphes-3.4.2/external/')
@@ -17,6 +20,9 @@ ROOT.gInterpreter.Declare('#include "/usr/local/Delphes-3.4.2/external/ExRootAna
 ROOT.gInterpreter.Declare('#include "/usr/local/Delphes-3.4.2/external/ExRootAnalysis/ExRootConfReader.h"')
 ROOT.gInterpreter.Declare('#include "/usr/local/Delphes-3.4.2/external/ExRootAnalysis/ExRootTask.h"')
 ROOT.gSystem.Load("/usr/local/Delphes-3.4.2/install/lib/libDelphes")
+
+MAX_JETS = 300
+N_CORES = 8
 
 
 def create_dataset(f, nevent, MAX_JETS):
@@ -80,67 +86,88 @@ def get_pt_eta_phi(constituents):
     return pts, etas, phis
 
 
-def from_root_to_h5(tree, output_path, nevent, signal):
-    # Select events and save the jets information to h5 file
-
+def select_event(root_path, sample_type, start, end):
     # Hidden Valley model selection
     # 1. 2 jets
     # 2. pT > 750 GeV
     # 3. |eta| < 2.0
     # 4. 4300 < Mjj < 5900 GeV
 
+    f = ROOT.TFile(root_path)
+    tree = f.Get("Delphes")
+
+    data_list = []
+    for i in tqdm(range(start, end)):
+        tree.GetEntry(i)
+
+        if tree.Jet_size < 2:
+            continue
+
+        if tree.Jet[1].PT < 750:
+            continue
+
+        if abs(tree.Jet[0].Eta) > 2.0 or abs(tree.Jet[1].Eta) > 2.0:
+            continue
+
+        jets = [[tree.Jet[i].PT, tree.Jet[i].Eta, tree.Jet[i].Phi, tree.Jet[i].Mass] for i in range(2)]
+        mjj = Mjets(*jets)
+
+        if mjj < 4300 or mjj > 5900:
+            continue
+
+        # get jet constituents
+        constituents = [consti for consti in tree.Jet[0].Constituents if consti != 0]
+        n_consti_1 = len(constituents)
+        PT1, Eta1, Phi1 = get_pt_eta_phi(constituents)
+
+        constituents = [consti for consti in tree.Jet[1].Constituents if consti != 0]
+        n_consti_2 = len(constituents)
+        PT2, Eta2, Phi2 = get_pt_eta_phi(constituents)
+
+        if n_consti_1 < 5 or n_consti_2 < 5:
+            continue
+
+        # 準備寫入資料
+        data_dict = {
+            'J1/MASK': np.arange(MAX_JETS) < n_consti_1,
+            'J1/pt': PT1[:MAX_JETS] if n_consti_1 > MAX_JETS else np.pad(PT1, (0, MAX_JETS-n_consti_1)),
+            'J1/eta': Eta1[:MAX_JETS] if n_consti_1 > MAX_JETS else np.pad(Eta1, (0, MAX_JETS-n_consti_1)),
+            'J1/phi': Phi1[:MAX_JETS] if n_consti_1 > MAX_JETS else np.pad(Phi1, (0, MAX_JETS-n_consti_1)),
+
+            'J2/MASK': np.arange(MAX_JETS) < n_consti_2,
+            'J2/pt': PT2[:MAX_JETS] if n_consti_2 > MAX_JETS else np.pad(PT2, (0, MAX_JETS-n_consti_2)),
+            'J2/eta': Eta2[:MAX_JETS] if n_consti_2 > MAX_JETS else np.pad(Eta2, (0, MAX_JETS-n_consti_2)),
+            'J2/phi': Phi2[:MAX_JETS] if n_consti_2 > MAX_JETS else np.pad(Phi2, (0, MAX_JETS-n_consti_2)),
+
+            'EVENT/Mjj': mjj,
+            'EVENT/signal': sample_type,
+        }
+        data_list.append(data_dict)
+
+    return data_list
+
+
+def from_root_to_h5(root_path, output_path, sample_type):
+    # Select events and save the jets information to h5 file
+
+    # Get number of events
+    f = ROOT.TFile(root_path)
+    nevent = f.Get("Delphes").GetEntries()
+    print(f'Number of events: {nevent}')
+
+    # Multi-core processing
+    print(f'Number of cores: {N_CORES}')
+    start = [nevent // N_CORES * i for i in range(N_CORES)]
+    end = [nevent // N_CORES * (i+1) for i in range(N_CORES)]
+    end[-1] = nevent
+
+    with mp.Pool(processes=N_CORES) as pool:
+        results = pool.starmap(select_event, zip(repeat(root_path), repeat(sample_type), start, end))
+    data_list = [data_dict for result_list in results for data_dict in result_list]
+
+    # write to h5 file
     with h5py.File(output_path, 'w') as f_out:
-
-        MAX_JETS = 300
         create_dataset(f_out, nevent, MAX_JETS)
-
-        data_list = []
-        for event_id, event in tqdm(enumerate(tree)):
-
-            if event.Jet_size < 2:
-                continue
-
-            if event.Jet[1].PT < 750:
-                continue
-
-            if abs(event.Jet[0].Eta) > 2.0 or abs(event.Jet[1].Eta) > 2.0:
-                continue
-
-            jets = [[event.Jet[i].PT, event.Jet[i].Eta, event.Jet[i].Phi, event.Jet[i].Mass] for i in range(2)]
-            mjj = Mjets(*jets)
-
-            if mjj < 4300 or mjj > 5900:
-                continue
-
-            # get jet constituents
-            constituents = [consti for consti in event.Jet[0].Constituents if consti != 0]
-            n_consti_1 = len(constituents)
-            PT1, Eta1, Phi1 = get_pt_eta_phi(constituents)
-
-            constituents = [consti for consti in event.Jet[1].Constituents if consti != 0]
-            n_consti_2 = len(constituents)
-            PT2, Eta2, Phi2 = get_pt_eta_phi(constituents)
-
-            if n_consti_1 < 5 or n_consti_2 < 5:
-                continue
-
-            # 準備寫入資料
-            data_dict = {
-                'J1/MASK': np.arange(MAX_JETS) < n_consti_1,
-                'J1/pt': PT1[:MAX_JETS] if n_consti_1 > MAX_JETS else np.pad(PT1, (0, MAX_JETS-n_consti_1)),
-                'J1/eta': Eta1[:MAX_JETS] if n_consti_1 > MAX_JETS else np.pad(Eta1, (0, MAX_JETS-n_consti_1)),
-                'J1/phi': Phi1[:MAX_JETS] if n_consti_1 > MAX_JETS else np.pad(Phi1, (0, MAX_JETS-n_consti_1)),
-
-                'J2/MASK': np.arange(MAX_JETS) < n_consti_2,
-                'J2/pt': PT2[:MAX_JETS] if n_consti_2 > MAX_JETS else np.pad(PT2, (0, MAX_JETS-n_consti_2)),
-                'J2/eta': Eta2[:MAX_JETS] if n_consti_2 > MAX_JETS else np.pad(Eta2, (0, MAX_JETS-n_consti_2)),
-                'J2/phi': Phi2[:MAX_JETS] if n_consti_2 > MAX_JETS else np.pad(Phi2, (0, MAX_JETS-n_consti_2)),
-
-                'EVENT/Mjj': mjj,
-                'EVENT/signal': signal,
-            }
-
-            data_list.append(data_dict)
         write_dataset(f_out, data_list)
 
 
@@ -150,14 +177,7 @@ def main():
     output_path = sys.argv[2]
     sample_type = int(sys.argv[3])
 
-    f = ROOT.TFile(root_file)
-    tree_s = f.Get("Delphes")
-
-    # Get number of events
-    nevent = tree_s.GetEntries()
-    print('Number of events: ', nevent)
-
-    from_root_to_h5(tree_s, output_path, nevent, sample_type)
+    from_root_to_h5(root_file, output_path, sample_type)
 
 
 if __name__ == '__main__':
