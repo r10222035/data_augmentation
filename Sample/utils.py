6a9c3ee6bfee4c2ad1c0624df37c6390b218c34b
Feng-Yang Hsieh <f10222035@ntu.edu.tw>
2024-05-29 16:00:34 +0800

    Code: Implement the preprocessing with only pT centralization

--- Sample/utils.py
+++ Sample/utils.py
@@ -30,7 +30,34 @@ def quadrant_max_vectorized(eta, phi, pt):
 def rotation(etas, phis, angles):
     # etas, phis are the coordinates
     # angles: rotation angle
-    eta_rotat, phi_rotat = etas * np.cos(angles) - phis * np.sin(angles), phis * np.cos(angles) + etas * np.sin(angles)
+    eta_rotat = etas * np.cos(angles) - phis * np.sin(angles)
+    phi_rotat = phis * np.cos(angles) + etas * np.sin(angles)
+
+    return eta_rotat, phi_rotat
+
+
+def shift_to_pt_center(pts, etas, phis):
+    # compute pt weighted center
+    # eta_central shape: (n_events, 1)
+    pt_sum = pts.sum(axis=1)
+    eta_central = ((pts * etas).sum(axis=1) / pt_sum)[:, None]
+    phi_central = ((pts * phis).sum(axis=1) / pt_sum)[:, None]
+
+    eta_shift, phi_shift = etas - eta_central, std_phi(phis - phi_central)
+
+    return eta_shift, phi_shift
+
+
+def rotate_to_pricipal_axis(pts, etas, phis):
+    # compute rotation angle
+    pt_sum = pts.sum(axis=1)
+    s_etaeta = (pts * etas**2).sum(axis=1) / pt_sum
+    s_phiphi = (pts * phis**2).sum(axis=1) / pt_sum
+    s_etaphi = (pts * etas * phis).sum(axis=1) / pt_sum
+
+    angle = -np.arctan2(-s_etaeta + s_phiphi + np.sqrt((s_etaeta - s_phiphi)**2 + 4. * s_etaphi**2), 2.*s_etaphi)[:, None]
+    eta_rotat, phi_rotat = rotation(etas, phis, angle)
+
     return eta_rotat, phi_rotat
 
 
@@ -40,21 +67,9 @@ def preprocess(pts, etas, phis):
     phis = np.where((variances > 0.5)[:, None], phis + np.pi, phis)
     phis = std_phi(phis)
 
-    # compute pt weighted center
-    # eta_central shape: (n_events, 1)
-    pt_sum = pts.sum(axis=1)
-    eta_central = ((pts * etas).sum(axis=1) / pt_sum)[:, None]
-    phi_central = ((pts * phis).sum(axis=1) / pt_sum)[:, None]
+    eta_shift, phi_shift = shift_to_pt_center(pts, etas, phis)
 
-    # compute rotation angle
-    s_etaeta = (pts * (etas - eta_central)**2).sum(axis=1) / pt_sum
-    s_phiphi = (pts * (phis - phi_central)**2).sum(axis=1) / pt_sum
-    s_etaphi = (pts * (etas - eta_central) * (phis - phi_central)).sum(axis=1) / pt_sum
-
-    angle = -np.arctan2(-s_etaeta + s_phiphi + np.sqrt((s_etaeta - s_phiphi)**2 + 4. * s_etaphi**2), 2.*s_etaphi)[:, None]
-
-    eta_shift, phi_shift = etas - eta_central, std_phi(phis - phi_central)
-    eta_rotat, phi_rotat = rotation(eta_shift, phi_shift, angle)
+    eta_rotat, phi_rotat = rotate_to_pricipal_axis(pts, eta_shift, phi_shift)
 
     pt_quadrants = quadrant_max_vectorized(eta_rotat, phi_rotat, pts)
 
@@ -65,3 +80,14 @@ def preprocess(pts, etas, phis):
     phi_news = phi_rotat * phi_flip
 
     return pts, eta_news, phi_news
+
+
+def preprocess_only_centralization(pts, etas, phis):
+
+    variances = np.var(phis, axis=1)
+    phis = np.where((variances > 0.5)[:, None], phis + np.pi, phis)
+    phis = std_phi(phis)
+
+    eta_shift, phi_shift = shift_to_pt_center(pts, etas, phis)
+
+    return pts, eta_shift, phi_shift
